2366d6b1374ef4bc9f99dada4051144317eb0e55
Nuradianto4 <adhisopo44@gmail.com>
2020-08-06 00:07:50 +0700

    templating blade selesai

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,4 +10,14 @@ class HomeController extends Controller
     {
     	return view('home');
     }
+
+    public function tabel()
+    {
+    	return view('items/tabel');
+    }
+
+    public function datatable()
+    {
+    	return view('items/data-table');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,19 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', 'HomeController@home');
+*/
+Route::get('/','HomeController@tabel');
 
 Route::get('/register','AuthController@register');
 Route::post('/register/proses','AuthController@proses');
 Route::get('/welcome','AuthController@welcome');
+Route::get('/master',function(){
+	return view('adminlte/master');
+});
+Route::get('/coba',function(){
+	return view('items/index');
+});
+
+Route::get('/data-table','HomeController@datatable');
